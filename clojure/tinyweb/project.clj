19e87860d7d93ea0fd82f80810376bda04f0e1ee
Larry Jones <mrwizard82d1@gmail.com>
2018-03-25 11:54:14 -0500

    Add src/clj and src/java to project source paths

--- clojure/tinyweb/project.clj
+++ clojure/tinyweb/project.clj
@@ -1,9 +1,11 @@
 (defproject tinyweb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "The tinyweb application from 'Functional Programming Patterns in Scala and Clojure.'"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot tinyweb.core
   :target-path "target/%s"
+  :source-paths ["src/clj"]
+  :java-source-path ["src/java"]
   :profiles {:uberjar {:aot :all}})
