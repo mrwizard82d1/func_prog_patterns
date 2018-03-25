994f68e9821d76c7d92be6ad636201f01c4340bd
Larry Jones <mrwizard82d1@gmail.com>
2018-03-25 16:04:31 -0500

    Add tinyweb function; test execute-request

--- clojure/tinyweb/src/clj/tinyweb/core.clj
+++ clojure/tinyweb/src/clj/tinyweb/core.clj
@@ -1,25 +1,59 @@
 (ns tinyweb.core
   (:gen-class)
-  (:import (com.cjl_magistri.tinyweb RenderingException)))
+  (:import (com.cjl_magistri.tinyweb RenderingException ControllerException)))
 
-(defn proto-controller [http-request]
-      {:name (http-request :body)})
-
-(defn proto-view [model]
-  (str "<h1>Hello, " (model :name) "</h1>"))
-
-(defn render [view model]
+(defn- render [view model]
   (try
     (view model)
     (catch Exception e
       (throw (RenderingException. e)))))
 
+(defn- execute-request [http-request handler]
+  (let [controller (handler :controller)
+        view (handler :view)]
+    (try
+      {:status-code 200
+       :body (render view
+               (controller http-request))}
+     (catch ControllerException e
+       {:status-code (.getStatusCode e)
+        :body ""})
+     (catch RenderingException e
+       {:status-code 500
+        :body "Exception while rendering"})
+     (catch Exception e
+       (.printStackTrace e)
+       {:status-code 500
+        :body 500}))))
+
+(defn- apply-filters [filters http-request]
+  (let [composed-filters (reduce comp (reverse filters))]
+    (composed-filters http-request)))
+
+(defn tinyweb [request-handlers filters]
+  (fn [http-request]
+    (let [filtered-request (apply-filters filters http-request)
+          path (http-request :path)
+          handler (request-handlers path)]
+      (execute-request filtered-request handler))))
+
+(defn- test-controller [http-request]
+  {:name (http-request :body)})
+
+(defn- test-view [model]
+  (str "<h1>Hello, ", (model :name) "</h1>"))
+
+(def test-request-handler {:controller test-controller
+                           :view test-view})
+
+(def test-http-request {:body "Mike"
+                        :path "/say-hello"
+                        :headers {}})
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (println (str "(proto-controller {:body \"Mike\"}): " (proto-controller {:body "Mike"})))
-  (println (str "(proto-view (proto-controller {:body \"Mike\"})): "
-                (proto-view (proto-controller {:body "Mike"}))))
-  (println (str "(render proto-view (proto-controller {:body \"Mike\"})): "
-                (render proto-view (proto-controller {:body "Mike"})))))
+  (println (str "(execute-request test-http-request test-request-handler): "
+                (execute-request test-http-request test-request-handler))))
+
